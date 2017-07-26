c20318d98b908e097b9495c89612e3c76b92b911
juhee <jh828204@naver.com>
2017-07-26 23:09:14 +0900

    초기 커밋.

---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(function () {
+
+    $(".slide-new").jCarouselLite({
+        auto: 800,
+        speed: 1500,
+        mouseWheel: true
+    });
+
+    $(".slide-banner").jCarouselLite({
+        auto: 800,
+        speed: 2500
+    });
+
+    $('.special.cards .image').dimmer({
+        on: 'hover'
+    });
+
+    $(".click-modal").click(function () {
+        var image = $(this).data("image");
+        var name = $(this).find("img").attr("alt");
+        var price = $(this).find(".date").text();
+
+        $(".price").text(price);
+        $("#title").text(name);
+        $("#modal-photo").attr("src", image);
+        $('.modal')
+            .modal({ allowMultiple: false })
+            .modal('show')
+            .modal('attach events', '.ui.button');
+    });
+});
+
+
